aa98f1a7deb55c2712b83a614a12dc2a016cc70f
Artem Kutsan <artemkutsan@gmail.com>
2026-05-30 21:24:52 +0200

    chore: добавлен маршрут /categories и заглушка страницы категорий

--- src/shared/config/routerConfig.jsx
+++ src/shared/config/routerConfig.jsx
@@ -1,11 +1,13 @@
 import MainPage from '../../pages/MainPage';
-import NotFoundPage from '../../pages/NotFoundPage';
 import RecipePage from '../../pages/RecipePage';
+import CategoriesPage from '../../pages/CategoriesPage';
 import RecipePageDetail from '../../pages/RecipePageDetails';
+import NotFoundPage from '../../pages/NotFoundPage';
 
 export const AppRouter = {
   MAIN: 'main',
   RECIPE: 'recipe',
+  CATEGORIES: 'categories',
   RECIPE_DETAIL: 'recipe_detail',
   NOT_FOUND: 'not_found',
 };
@@ -13,6 +15,7 @@ export const AppRouter = {
 export const RouterPath = {
   [AppRouter.MAIN]: '/',
   [AppRouter.RECIPE]: '/recipe',
+  [AppRouter.CATEGORIES]: '/categories',
   [AppRouter.RECIPE_DETAIL]: '/recipe/:id',
   [AppRouter.NOT_FOUND]: '*',
 };
@@ -25,6 +28,10 @@ export const routeConfig = {
   [AppRouter.RECIPE]: {
     path: RouterPath.recipe,
     element: <RecipePage />,
+  },
+  [AppRouter.CATEGORIES]: {
+    path: RouterPath.categories,
+    element: <CategoriesPage />,
   },
   [AppRouter.RECIPE_DETAIL]: {
     path: RouterPath.recipe_detail,
