e4858af5a75ddd64c0b4610b4801d833ba2c3bc1
Artem Kutsan <artemkutsan@gmail.com>
2026-05-31 15:25:53 +0200

    feat: установлены библиотеки redux и RTK, создан и подключен store

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import App from '@/app/App.jsx';
 import '@/app/styles/index.css';
+import { store } from '@/app/providers/store';
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
 );
