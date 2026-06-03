5518cc9c28f6775d6da4671238687bda84cd0bde
Artem Kutsan <artemkutsan@gmail.com>
2026-06-03 11:18:15 +0200

    feat: переход на загрузку рецептов через store на RecipePage с использованием axios

--- src/entities/recipe/model/thunks.js
+++ src/entities/recipe/model/thunks.js
@@ -1,8 +1,8 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
 
 export const fetchRecipes = createAsyncThunk('recipes/fetchRecipes', async () => {
-  const response = await fetch('https://dummyjson.com/recipes');
-  const data = await response.json();
+  const { data } = await axios.get('https://dummyjson.com/recipes');
 
   return data.recipes;
 });
--- src/pages/RecipePage/index.jsx
+++ src/pages/RecipePage/index.jsx
@@ -1,20 +1,24 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { RecipeList } from '@/entities/recipe/ui';
+import { fetchRecipes } from '@/entities/recipe/model/thunks';
+import {
+  selectRecipes,
+  selectRecipesError,
+  selectRecipesStatus,
+} from '@/entities/recipe/model/selectors';
 
 const RecipePage = () => {
-  const [recipes, setRecipes] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+  const dispatch = useDispatch();
+  const recipes = useSelector(selectRecipes);
+  const status = useSelector(selectRecipesStatus);
+  const error = useSelector(selectRecipesError);
 
   useEffect(() => {
-    fetch('https://dummyjson.com/recipes')
-      .then((res) => res.json())
-      .then((data) => setRecipes(data.recipes))
-      .catch(() => setError('Failed to load recipes'))
-      .finally(() => setLoading(false));
-  }, []);
+    dispatch(fetchRecipes());
+  }, [dispatch]);
 
-  if (loading) return <p>Loading...</p>;
+  if (status === 'loading') return <p>Loading...</p>;
   if (error) return <p>{error}</p>;
 
   return (
