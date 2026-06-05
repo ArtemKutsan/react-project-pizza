d38fb7d7cb07839b76ed06c8859f605b84020f71
Artem Kutsan <artemkutsan@gmail.com>
2026-06-05 18:22:05 +0200

    chore: незначительные обновления стилей

--- src/shared/ui/Badge/index.jsx
+++ src/shared/ui/Badge/index.jsx
@@ -2,7 +2,9 @@ import { cn } from '@/shared/lib/cn';
 
 const Badge = ({ children, className = '' }) => {
   return (
-    <span className={cn('rounded-full bg-emerald-50 px-4 py-2 text-sm text-lime-700', className)}>
+    <span
+      className={cn('w-fit rounded-full bg-emerald-50 px-4 py-2 text-sm text-lime-700', className)}
+    >
       {children}
     </span>
   );
