a3fa2d5d2abb32ae3b8134b8624e1f458a4ffd63
Artem Kutsan <artemkutsan@gmail.com>
2026-06-08 13:45:44 +0200

    feat: добавлена в список навигации сайдбара ссылка на страницу добавления нового рецепта

--- src/widgets/Sidebar/navItems.js
+++ src/widgets/Sidebar/navItems.js
@@ -1,8 +1,12 @@
-import { RouterPath } from "@/shared/config/routerConfig";
-import HomeIcon from "@/assets/icons/home.svg?react";
-import CategoriesIcon from "@/assets/icons/categories.svg?react";
+// src/widgets/Sidebar/navItems.js
+import { RouterPath } from '@/shared/config/routerConfig';
+import HomeIcon from '@/assets/icons/home.svg?react';
+import CategoriesIcon from '@/assets/icons/categories.svg?react';
+import ListIcon from '@/assets/icons/list.svg?react';
 
+// Конфигурация навигационных пунктов для сайдбара, которая включает путь, метку и иконку для каждого пункта
 export const navItems = [
-  { to: RouterPath.recipes, label: "Recipes", Icon: HomeIcon },
-  { to: RouterPath.categories, label: "Categories", Icon: CategoriesIcon },
+  { to: RouterPath.recipes, label: 'Recipes', Icon: HomeIcon },
+  { to: RouterPath.categories, label: 'Categories', Icon: CategoriesIcon },
+  { to: RouterPath.meal_planner, label: 'Meal Planner', Icon: ListIcon },
 ];
