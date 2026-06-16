c41cdcb0c635e277a25a22cd6baf5efe3041690c
Artem Kutsan <artemkutsan@gmail.com>
2026-06-16 17:53:23 +0200

    fix: изменение/уточнение классов tailwind под новые цветовые токены ткмы

--- src/shared/ui/Button/index.jsx
+++ src/shared/ui/Button/index.jsx
@@ -1,19 +1,20 @@
 import { cn } from '@/shared/lib/cn';
 
 const buttonVariants = {
-  default: 'bg-lime-700 text-white hover:bg-lime-800',
-  destructive: 'bg-rose-600 text-white hover:bg-rose-700',
-  outline: 'border border-slate-200 bg-white text-slate-900 hover:bg-slate-50',
-  secondary: 'bg-slate-100 text-slate-900 hover:bg-slate-200',
-  ghost: 'bg-transparent text-slate-900 hover:bg-slate-100',
-  link: 'bg-transparent text-lime-700 underline-offset-4 hover:underline',
+  default: 'bg-primary text-primary-foreground hover:bg-primary/87.5',
+  destructive:
+    'bg-destructive/10 dark:bg-destructive/20 text-destructive hover:bg-destructive/20 dark:hover:bg-destructive/30',
+  outline: 'border hover:bg-lite hover:text-accent-foreground',
+  secondary: 'bg-secondary text-secondary-foreground hover:bg-secondary/87.5',
+  ghost: 'hover:bg-accent hover:text-accent-foreground',
+  link: 'text-primary underline-offset-4 hover:underline',
 };
 
 const buttonSizes = {
-  default: 'min-h-10 min-w-10 px-4 py-2 text-base has-[>svg]:px-3',
-  xs: "min-h-6 min-w-6 gap-1 rounded-lg px-2 text-xs has-[>svg]:px-1.5 [&_svg:not([class*='size-'])]:size-3",
-  sm: 'min-h-8 min-w-8 gap-1.5 rounded-xl px-3 py-1.5 text-sm has-[>svg]:px-2.5',
-  lg: 'min-h-12 min-w-12 gap-3 px-6 py-2.5 rounded-2xl text-lg has-[>svg]:px-4',
+  default: 'min-h-10 min-w-10 px-4 has-[>svg]:px-3',
+  xs: "min-h-6 min-w-6 gap-1 rounded-sm px-2 text-xs has-[>svg]:px-1.5 [&_svg:not([class*='size-'])]:size-3",
+  sm: 'min-h-8 min-w-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5',
+  lg: 'min-h-12 min-w-12 rounded-xl px-6 has-[>svg]:px-4 gap-3',
   icon: 'size-10 rounded-full',
 };
 
@@ -28,7 +29,7 @@ const Button = ({
   return (
     <Component
       className={cn(
-        'inline-flex w-fit items-center justify-center gap-2 rounded-xl font-medium transition-colors disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*="size-"])]:size-4',
+        'inline-flex items-center justify-center gap-2 cursor-pointer whitespace-nowrap rounded-lg text-sm font-medium transition-colors disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*="size-"])]:size-4 shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive',
         buttonVariants[variant],
         buttonSizes[size],
         className,
